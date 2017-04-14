9158f6c9cb52469cfcc55eddc01f7f51fc191887
Kuya Takami <kuya@gwu.edu>
2017-04-13 21:59:21 -0400

    initiate reorganization

---
+++ scripts/mission_control.py
@@ -0,0 +1,7 @@
+#!~/anaconda3/bin/python
+from __future__ import print_function, division
+
+# implement mode switching of the controller when the vicon signal is not updating
+from odroid import *
+print('system starting')
+
